49828f42b0f4ac8126271441c573a107eb58fd47
Leader <abdo.gammal1233@gmail.com>
2024-07-22 10:07:15 +0300

    last

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className='bg-red-400'>
+      <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
